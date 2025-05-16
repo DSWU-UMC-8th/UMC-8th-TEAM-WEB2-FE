c8dd152bdf9cf953072210d67e27e6966299c373
jinhyo <kjhyo0220@gmail.com>
2025-05-16 16:21:54 +0900

    💡 Add: 폰트 및 기본 컬러 설정 추가 (#1)

---
+++ ReviewInClass-FE/src/styles/theme.ts
@@ -0,0 +1,24 @@
+const palette = {
+  primary: {
+    primaryDark: "#6FA235",
+    primary: "#B4D780",
+    primaryLight: "#CAE3A5",
+  },
+  secondary: {
+    secondaryDark: "#00AC49",
+    secondary: "#69C57E",
+    secondaryLight: "#C0E5C7",
+  },
+  point: "#26CF9F",
+  gray: {
+    gray900: "#2B2B2B",
+    gray700: "#6D6D6D",
+    gray500: "#B5B5B5",
+    gray300: "#D9D9D9",
+    gray200: "#E9E9E9",
+  },
+  white: "#ffffff",
+  background: "#F5F5F7",
+};
+
+export default palette;
