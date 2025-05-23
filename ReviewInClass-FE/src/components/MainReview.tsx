f450eefde92df85ab7e873652299f8a12850e8d3
jinhyo <kjhyo0220@gmail.com>
2025-05-23 16:18:32 +0900

    ✨ Feat: 메인 페이지 인기 리뷰 구현 (#7)
    메인 페이지 인기 리뷰 구현

---
+++ ReviewInClass-FE/src/components/MainReview.tsx
@@ -0,0 +1,57 @@
+import Star from "./common/Star";
+
+import Profile from "../assets/profile.png";
+import palette from "../styles/theme";
+
+interface Review {
+  id: number;
+  title: string; // 강의명
+  rating: number; // 별점
+  content: string; // 리뷰 내용
+  name: string; // 강사명
+}
+
+interface MainReviewProps {
+  reviews: Review[];
+}
+
+const MainReview = ({ reviews }: MainReviewProps) => {
+  const sortedReview = [...reviews].sort((a, b) => b.rating - a.rating).slice(0, 5); // 별점 높은 순 5개
+
+  return (
+    <>
+      <div
+        className="flex gap-[24px] w-full overflow-x-scroll"
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+      >
+        {sortedReview.map((review) => {
+          return (
+            <div
+              key={review.id}
+              className="flex flex-col gap-[13px] p-[32px] min-w-[376px] w-full h-[232px] rounded-[24px] rounded-bl-[4px]"
+              style={{ background: palette.white }}
+            >
+              <div className="font-semibold text-[20px] leading-[33.66px] tracking-[-1%]">{review.title}</div>
+              <Star star={review.rating} width={25.4} gap={8.89} />
+              <div
+                className="font-semibold text-[15px] leading-[22px] tracking-[-1%]"
+                style={{ color: palette.gray.gray700 }}
+              >
+                {review.content}
+              </div>
+
+              <div className="flex gap-[12px]">
+                <div className="w-[48px] h-[48px] rounded-[50%] overflow-hidden">
+                  <img src={Profile} alt="강사사진" className="w-full h-full object-cover" />
+                </div>
+                <div className="font-semibold text-[15px] leading-[33.66px] tracking-[-1%]">{review.name}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default MainReview;
--- ReviewInClass-FE/src/pages/Main.tsx
+++ ReviewInClass-FE/src/pages/Main.tsx
@@ -1,11 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import Banner from "../components/common/Banner/Banner";
+import MainReview from "../components/MainReview";
 
 import { LECTURE } from "../data/banner";
+import { mainDummyReview as REVIEWS } from "../data/mainDummyReview";
+import palette from "../styles/theme";
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  const handlePopularClick = () => {
+    navigate("review?sort=popular");
+  };
   return (
-    <div>
+    <div className="mt-[40px]">
       <Banner lectures={LECTURE.slice(0, 4)} />
+
+      <div className="pl-[78px] pr-[78px] mt-[80px]">
+        <div className="flex justify-between">
+          <h3 className="font-medium text-[25px] leading-[100%] tracking-[0%]" style={{ color: palette.gray.gray900 }}>
+            인기 리뷰
+          </h3>
+          <div
+            onClick={handlePopularClick}
+            className="font-semibold text-[20px] leading-[100%] tracking-[0%] cursor-pointer"
+            style={{ color: palette.gray.gray700 }}
+          >
+            전체 보기 {">"}
+          </div>
+        </div>
+      </div>
+
+      <div className="pl-[78px] mt-[20px]">
+        <MainReview reviews={REVIEWS} />
+      </div>
     </div>
   );
 };
